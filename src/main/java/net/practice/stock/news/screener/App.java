4ce069ba5d67387084a5eaa5bf6f0e8173bf2643
priyanka mali <priyanka.k.shelar@gmail.com>
2017-08-27 17:58:58 +0800

    spring boot hello

---
+++ src/main/java/net/practice/stock/news/screener/App.java
@@ -0,0 +1,13 @@
+package net.practice.stock.news.screener;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
